bed5aedcb4029d067ab2e94c90c369bf7734dbe0
ManeeshKrishna724 <91873467+ManeeshKrishna724@users.noreply.github.com>
2023-06-11 21:31:46 +0530

    nineth

--- portfolio_main/portfolio/models.py
+++ portfolio_main/portfolio/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
+    class Meta:
+        ordering = ["order_number"]
+
     name = models.CharField(max_length=200)
     demo = models.FileField(upload_to="demos")
+    order_number = models.IntegerField(null=True)
     description = models.TextField()
     project_url = models.URLField(blank=True)
     created_date = models.DateField()
